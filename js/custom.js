a2794dbc98e7dbae42f1f36c70ded12e407f44c9
Kyleigh Teeters <khess95@hotmail.com>
2024-03-14 20:41:44 -0400

    updated js

--- js/custom.js
+++ js/custom.js
@@ -1,41 +1,39 @@
-var NumberOfWords = 10
-
-var words = new BuildArray(NumberOfWords)
-
-// Use the following variables to 
-// define your random words:
-words[1] = "California"
-words[2] = "Paris"
-words[3] = "Madagascar"
-words[4] = "Japan"
-words[5] = "Mexico"
-words[6] = "Toronto"
-words[7] = "Barcelona"
-words[8] = "Rome"
-words[9] = "Thailand"
-words[10] = "Spain"
-
-function BuildArray(size){
-this.length = size
-for (var i = 1; i <= size; i++){
-this[i] = null}
-return this
-}
-
-function PickRandomWord(frm) {
-// Generate a random number between 1 and NumberOfWords
-var rnd = Math.ceil(Math.random() * NumberOfWords)
-
-// Display the word inside the text box
-console.log(rnd)
-frm.WordBox.value = words[rnd]
-}
-
-
-
-
+// Content Block 7
 const container = document.querySelector('.hover-container');
 const text = document.querySelector('.hover-text');
+
+// Constant variable with empty array
+const locations = [];
+
+// Assigning elements in array
+locations[0] = "California"
+locations[1] = "Paris"
+locations[2] = "Madagascar"
+locations[3] = "Japan"
+locations[4] = "Mexico"
+locations[5] = "Toronto"
+locations[6] = "Barcelona"
+locations[7] = "Rome"
+locations[8] = "Thailand"
+locations[9] = "Spain"
+locations[10] = "Santorini"
+locations[11] = "Iceland"
+locations[12] = "Austrailia"
+
+// Generating random location from array
+function locationFinder(){
+  let length = locations.length;
+  let num = Math.floor(Math.random() * length);
+
+  // Connects HTML & JS
+  let destination = document.getElementById("compass");
+
+  // Populates p tag with random generated location 
+  // variable, attribute, value
+  destination.innerHTML = locations[num];
+}
+
+
 
 container.addEventListener('mouseenter', function () {
   text.style.opacity = '1'; // Show text on hover
--- js/test.js
+++ js/test.js
@@ -1,32 +1,50 @@
-var NumberOfWords = 10
 
-var words = new BuildArray(NumberOfWords)
 
-// Use the following variables to 
-// define your random words:
-words[1] = "California"
-words[2] = "Paris"
-words[3] = "Madagascar"
-words[4] = "Japan"
-words[5] = "Mexico"
-words[6] = "Toronto"
-words[7] = "Barcelona"
-words[8] = "Rome"
-words[9] = "Thailand"
-words[10] = "Spain"
 
-function BuildArray(size){
-this.length = size
-for (var i = 1; i <= size; i++){
-this[i] = null}
-return this
-}
 
-function PickRandomWord(frm) {
-// Generate a random number between 1 and NumberOfWords
-var rnd = Math.ceil(Math.random() * NumberOfWords)
 
-// Display the word inside the text box
-console.log(rnd)
-frm.WordBox.value = words[rnd]
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var NumberOfWords = 10
+
+// var words = new BuildArray(NumberOfWords)
+
+// // Use the following variables to 
+// // define your random words:
+// words[1] = "California"
+// words[2] = "Paris"
+// words[3] = "Madagascar"
+// words[4] = "Japan"
+// words[5] = "Mexico"
+// words[6] = "Toronto"
+// words[7] = "Barcelona"
+// words[8] = "Rome"
+// words[9] = "Thailand"
+// words[10] = "Spain"
+
+// function BuildArray(size){
+// this.length = size
+// for (var i = 1; i <= size; i++){
+// this[i] = null}
+// return this
+// }
+
+// function PickRandomWord(frm) {
+// // Generate a random number between 1 and NumberOfWords
+// var rnd = Math.ceil(Math.random() * NumberOfWords)
+
+// // Display the word inside the text box
+// console.log(rnd)
+// frm.WordBox.value = words[rnd]
+// }
